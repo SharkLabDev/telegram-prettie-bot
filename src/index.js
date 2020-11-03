13db3e7f44ec5ed5e4dd604cdf0fd82ab2adfe65
Juan Sebastián Agudelo <Juansac.me@gmail.com>
2020-11-02 19:35:06 -0500

    [Add] Ajustes a los comandos y se agregaron nuevos.
    Ajustes al comando inical.
    Responde con tu nombre, no con tu arroba

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, {polling:true});
 
+// Create a bot that uses 'polling' to fetch new updates
 bot.on('polling_error', error => console.log(error))
 
 bot.onText(/^\/start/, msg => {
   console.log(msg)
   let chatId = msg.chat.id;
   let userName = msg.from.username;
-
-  bot.sendMessage(chatId, `Bienvenido a mi bot @${userName}`)
+  bot.sendMessage(chatId, `Yes! I'm already alive! @${userName}`)
 })
 
+bot.onText(/^\/chatid/, msg => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, `El id de este chat es: ${chatId}`);
+});
+
+bot.onText(/^\/myid/, msg => {
+  const chatId = msg.chat.id;
+  const myId = msg.from.id;
+  bot.sendMessage(chatId, `Tu id es: ${myId}`);
+});
+
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+  const myId = msg.from.id;
+  const userName = msg.from.username;
+  const name = msg.from.first_name;
+  if (msg.text.toLowerCase() === 'hola bot') {
+    console.log(msg)
+    bot.sendMessage(chatId, `Hola ${name}. ¿Cómo estas?`)
+  }
+})
